9e2354be5c3e8787871fcb3b0a83abe591fd3a37
fabricio <fabricio-fjc@outlook.com>
2024-04-17 21:40:10 -0300

    [refactor] etapa 1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { AppContext } from "../contexts/AppContext";
+import { AppContext } from "../../contexts/AppContext";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "../pages/Home";
-import Login from "../pages/Login";
-import ItemDisplay from "../pages/ItemDisplay";
-import Search from "../pages/Search";
+import Home from "../../pages/Home";
+import Login from "../../pages/Login";
+import ItemDisplay from "../../pages/ItemDisplay";
+import Search from "../../pages/Search";
 
 export default function App() {
   return (
